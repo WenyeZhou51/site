ed5ca62a0b6f3d1ee3f5058b294d58f3582b07a0
Wenye Zhou <wz51@rice.edu>
2024-11-12 21:03:40 -0600

    Updates

---
+++ precache-manifest.e36fb9e790243abe163814e3b27cc601.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67c9c765ce8ba4ffdcff8583000a18f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "681de582aae5a6564a51",
+    "url": "/static/css/main.427275ce.chunk.css"
+  },
+  {
+    "revision": "0732d704ec3269465de0",
+    "url": "/static/js/2.3998afa7.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.3998afa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd536d7e87c784d7005b",
+    "url": "/static/js/3.6687687a.chunk.js"
+  },
+  {
+    "revision": "681de582aae5a6564a51",
+    "url": "/static/js/main.b1bd55f6.chunk.js"
+  },
+  {
+    "revision": "0c33364db4f8141de5bc",
+    "url": "/static/js/runtime-main.9182d6b4.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
