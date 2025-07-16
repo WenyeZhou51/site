c6093515054647ad7674d3aef33bb60f0358298a
Wenye Zhou <wz51@rice.edu>
2025-07-16 13:29:37 +0800

    Updates

---
+++ precache-manifest.17647a43ccf54fcd852c9deb2d7b86b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ec556993110ce0f1c00afab3242d06f",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "6c0de1283a6940e1f7b5",
+    "url": "/site/static/css/2.31e1fcec.chunk.css"
+  },
+  {
+    "revision": "72d1fbca39cf913257b2",
+    "url": "/site/static/css/main.82685772.chunk.css"
+  },
+  {
+    "revision": "6c0de1283a6940e1f7b5",
+    "url": "/site/static/js/2.d1aea2f0.chunk.js"
+  },
+  {
+    "revision": "0f431788df9fedcd7536120fa5bb2b3b",
+    "url": "/site/static/js/2.d1aea2f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9dcb1137e6738447b27",
+    "url": "/site/static/js/3.bebbd9e9.chunk.js"
+  },
+  {
+    "revision": "72d1fbca39cf913257b2",
+    "url": "/site/static/js/main.c7bb777f.chunk.js"
+  },
+  {
+    "revision": "634dc00bc24fa95e3343",
+    "url": "/site/static/js/runtime-main.81066349.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d46f2960832b44bffac35bae6965d5a7.js"
+  "/site/precache-manifest.17647a43ccf54fcd852c9deb2d7b86b9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/site/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
